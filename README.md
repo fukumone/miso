7b4bf701760834ed4c62aa1b78f446468bff9523
t-fukui <fukuimone@gmail.com>
2015-09-21 13:12:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# miso
+
+WebSocket Demo Application
+
+# To Install and Run:
+
+```
+$ bundle install
+
+$ rackup
+# open localhost:9292
+```
---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra-websocket'
+
+class MisoApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  set :server, 'thin'
+  set :sockets, []
+
+  get '/' do
+    if !request.websocket?
+      erb :index
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws
+        end
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+      end
+    end
+  end
+
+end
