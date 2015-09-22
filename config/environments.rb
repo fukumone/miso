161be34a6170ca6e6ee531072d460cd409526d3b
t-fukui <fukuimone@gmail.com>
2015-09-22 16:02:30 +0900

    dotenv読み込み指定

--- config/environments.rb
+++ config/environments.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'json'
+require 'dotenv'
+
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 
 env_index = ARGV.index("-e")
 env_arg = ARGV[env_index + 1] if env_index
 env = env_arg || ENV["SINATRA_ENV"] || "development"
+
+Dotenv.load(".env.#{env}")
 
 use ActiveRecord::ConnectionAdapters::ConnectionManagement # close connection to the DDBB properly...https://github.com/puma/puma/issues/59
 databases = YAML.load_file("config/database.yml")
